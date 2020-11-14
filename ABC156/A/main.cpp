b66238d5ce9221d5f511f8367d4c04506572a2ca
marinebox <sh.25.tgt@gmail.com>
2020-11-14 20:50:53 +0900

    add new file

--- ABC156/A/main.cpp
+++ ABC156/A/main.cpp
@@ -16,5 +16,17 @@ using pii = pair<int, int>;
 
 
 int main(){
-    
+    int n, r;
+    cin >> n >> r;
+
+    int ans;
+    if(n >= 10){
+        ans = r;
+    }
+    else{
+        ans = 100 * (10 - n) + r;
+    }
+
+    cout << ans << endl;
+    return 0;
 }
