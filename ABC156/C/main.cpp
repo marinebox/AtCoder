550995baab7dccf239a44467decc508fc1913a74
marinebox <sh.25.tgt@gmail.com>
2020-11-14 21:37:19 +0900

    update

--- ABC156/C/main.cpp
+++ ABC156/C/main.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for(int i = 0; i < n; i++)
 #define vv(T) vector<vector<T>>
-#define coa cout << ans << endl
+#define coa cout << ans << endl;
+#define cinvec(vec) for(auto &i : vec) cin >> i
 using namespace std;
 using ll = long long;
 using vint = vector<int>;
@@ -16,5 +17,20 @@ using pii = pair<int, int>;
 
 
 int main(){
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    cinvec(x);
     
+    int ans = 1e9;
+    for(int i = 1; i <= 100; i++){
+        int tmp = 0;
+        for(auto j : x){
+            tmp += pow(i - j, 2);
+        }
+        ans = min(ans, tmp);
+    }
+
+    coa;
+    return 0;
 }
