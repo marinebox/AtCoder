9ce4ccce8c16c6f6e0e01bddfbfe7e49490a7bf4
marinebox <60165927+marinebox@users.noreply.github.com>
2021-10-04 03:08:45 +0900

    reduce using

--- my_template.cpp
+++ my_template.cpp
@@ -59,15 +59,15 @@
 using namespace std;
 // using namespace atcoder;
 using ll = __int64_t;
-template<typename T>
-using vec = vector<T>;
-template<typename T>
-using vvec = vector<vector<T>>;
-using qint = queue<int>;
-using sint = stack<int>;
-using pii = pair<int, int>;
-using dint  = deque<int>;
-using Graph = vector<vector<int>>;
+// template<typename T>
+// using vec = vector<T>;
+// template<typename T>
+// using vvec = vector<vector<T>>;
+// using qint = queue<int>;
+// using sint = stack<int>;
+// using pii = pair<int, int>;
+// using dint  = deque<int>;
+// using Graph = vector<vector<int>>;
 
 {% if mod %}
 const long long MOD = {{ mod }};
