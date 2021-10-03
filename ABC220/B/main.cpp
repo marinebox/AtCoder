1e46fdc777699226b11025ee880d01284a39c453
marinebox <60165927+marinebox@users.noreply.github.com>
2021-10-04 03:15:11 +0900

    edit

--- ABC220/B/main.cpp
+++ ABC220/B/main.cpp
@@ -1,38 +1,73 @@
-#include <bits/stdc++.h>
+// #include <algorithm>
+// #include <bitset>
+// #include <complex>
+// #include <deque>
+// #include <exception>
+// #include <fstream>
+// #include <functional>
+// #include <iomanip>
+// #include <ios>
+// #include <iosfwd>
+#include <iostream>
+// #include <istream>
+// #include <iterator>
+// #include <limits>
+// #include <list>
+// #include <locale>
+// #include <map>
+// #include <memory>
+// #include <new>
+// #include <numeric>
+// #include <ostream>
+// #include <queue>
+// #include <set>
+// #include <sstream>
+// #include <stack>
+// #include <stdexcept>
+// #include <streambuf>
+// #include <string>
+// #include <typeinfo>
+// #include <utility>
+// #include <valarray>
+// #include <vector>
+// #include <array>
+// #include <atomic>
+// #include <chrono>
+// #include <condition_variable>
+// #include <forward_list>
+// #include <future>
+// #include <initializer_list>
+// #include <mutex>
+// #include <random>
+// #include <ratio>
+// #include <regex>
+// #include <scoped_allocator>
+// #include <system_error>
+// #include <thread>
+// #include <tuple>
+// #include <typeindex>
+// #include <type_traits>
+// #include <unordered_map>
+// #include <unordered_set>
 // #include <atcoder/all>
 #define rep(i, n) for(int i = 0; i < n; i++)
-#define vv(T) vector<vector<T>>
-#define coa cout << ans << endl; return 0;
 #define cinvec(vec) for(auto &i : vec) cin >> i
 #define deg_to_rad(deg) ((deg) / 360) * 2 * M_PI
 #define rad_to_deg(rad) ((rad) / 2 / M_PI) * 360
 using namespace std;
 // using namespace atcoder;
 using ll = __int64_t;
-template<typename T>
-using vec = vector<T>;
-template<typename T>
-using vvec = vector<vector<T>>;
-using qint = queue<int>;
-using sint = stack<int>;
-using pii = pair<int, int>;
-using dint  = deque<int>;
-using Graph = vector<vector<int>>;
+// template<typename T>
+// using vec = vector<T>;
+// template<typename T>
+// using vvec = vector<vector<T>>;
+// using qint = queue<int>;
+// using sint = stack<int>;
+// using pii = pair<int, int>;
+// using dint  = deque<int>;
+// using Graph = vector<vector<int>>;
 
-ll ntod(int n, string k){
-    ll ret = 0;
-    for(auto c:k){
-        ret *= n;
-        ret += c - '0';
-    }
-    return ret;
-}
 
 int main(){
-    ll k; cin >> k;
-    string a, b; cin >> a >> b;
-    ll da = ntod(k, a), db = ntod(k, b);
-    ll ans = da * db;
-    cout << ans << endl;
-    return 0;
+    
 }
--- ABC220/D/main.cpp
+++ ABC220/D/main.cpp
@@ -1,50 +1,74 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
+// #include <algorithm>
+// #include <bitset>
+// #include <complex>
+// #include <deque>
+// #include <exception>
+// #include <fstream>
+// #include <functional>
+// #include <iomanip>
+// #include <ios>
+// #include <iosfwd>
+#include <iostream>
+// #include <istream>
+// #include <iterator>
+// #include <limits>
+// #include <list>
+// #include <locale>
+// #include <map>
+// #include <memory>
+// #include <new>
+// #include <numeric>
+// #include <ostream>
+// #include <queue>
+// #include <set>
+// #include <sstream>
+// #include <stack>
+// #include <stdexcept>
+// #include <streambuf>
+// #include <string>
+// #include <typeinfo>
+// #include <utility>
+// #include <valarray>
+// #include <vector>
+// #include <array>
+// #include <atomic>
+// #include <chrono>
+// #include <condition_variable>
+// #include <forward_list>
+// #include <future>
+// #include <initializer_list>
+// #include <mutex>
+// #include <random>
+// #include <ratio>
+// #include <regex>
+// #include <scoped_allocator>
+// #include <system_error>
+// #include <thread>
+// #include <tuple>
+// #include <typeindex>
+// #include <type_traits>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <atcoder/all>
 #define rep(i, n) for(int i = 0; i < n; i++)
-#define vv(T) vector<vector<T>>
-#define coa cout << ans << endl; return 0;
 #define cinvec(vec) for(auto &i : vec) cin >> i
 #define deg_to_rad(deg) ((deg) / 360) * 2 * M_PI
 #define rad_to_deg(rad) ((rad) / 2 / M_PI) * 360
 using namespace std;
-using namespace atcoder;
+// using namespace atcoder;
 using ll = __int64_t;
-template<typename T>
-using vec = vector<T>;
-template<typename T>
-using vvec = vector<vector<T>>;
-using qint = queue<int>;
-using sint = stack<int>;
-using pii = pair<int, int>;
-using dint  = deque<int>;
-using Graph = vector<vector<int>>;
+// template<typename T>
+// using vec = vector<T>;
+// template<typename T>
+// using vvec = vector<vector<T>>;
+// using qint = queue<int>;
+// using sint = stack<int>;
+// using pii = pair<int, int>;
+// using dint  = deque<int>;
+// using Graph = vector<vector<int>>;
 
 const long long MOD = 998244353;
-using mint = modint998244353;
-using mint10 = modint;
 
 int main(){
-    int n; cin >> n;
-    vector<int> a(n + 5);
-    // 1オリジンにする
-    for(int i = 1; i <= n; i++) cin >> a[i];
-
-    mint10::set_mod(10);
-    vector<vector<mint>> dp(n + 3, vector<mint>(13, 0));
-    dp[1][a[1]] = 1;
-    for(int i = 1; i <= n; i++){
-        // i個目まで見たとき、一番左の値がjになる組み合わせはいくつ存在するか。
-        for(int j = 0; j < 10; j++){
-            auto prev = dp[i][j];
-            int nextj1 = (j + a[i + 1]) % 10;
-            int nextj2 = (j * a[i + 1]) % 10;
-            dp[i + 1][nextj1] += prev;
-            dp[i + 1][nextj2] += prev;
-        }
-    }
-
-    for(int i = 0; i < 10; i++){
-        cout << dp[n][i].val() << endl;
-    }
-    return 0;
+    
 }
