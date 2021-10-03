aef8587d7a2159e656f16aaa34df5408344aceb5
marinebox <60165927+marinebox@users.noreply.github.com>
2021-10-04 03:00:21 +0900

    edit for clang

---
+++ my_template_gcc.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+// #include <atcoder/all>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define vv(T) vector<vector<T>>
+#define coa cout << ans << endl; return 0;
+#define cinvec(vec) for(auto &i : vec) cin >> i
+#define deg_to_rad(deg) ((deg) / 360) * 2 * M_PI
+#define rad_to_deg(rad) ((rad) / 2 / M_PI) * 360
+using namespace std;
+// using namespace atcoder;
+using ll = __int64_t;
+template<typename T>
+using vec = vector<T>;
+template<typename T>
+using vvec = vector<vector<T>>;
+using qint = queue<int>;
+using sint = stack<int>;
+using pii = pair<int, int>;
+using dint  = deque<int>;
+using Graph = vector<vector<int>>;
+
+{% if mod %}
+const long long MOD = {{ mod }};
+{% endif %}
+{% if yes_str %}
+const string YES = "{{ yes_str }}";
+{% endif %}
+{% if no_str %}
+const string NO = "{{ no_str }}";
+{% endif %}
+
+int main(){
+    
+}
